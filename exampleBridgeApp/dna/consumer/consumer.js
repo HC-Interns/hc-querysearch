4681f978dcb579e4218204d9f66ae81ca76f558d
Willem Olding <willemolding@gmail.com>
2018-07-03 13:06:02 -0600

    indexing success through messaging an indexer node

--- exampleBridgeApp/dna/consumer/consumer.js
+++ exampleBridgeApp/dna/consumer/consumer.js
@@ -7,26 +7,78 @@ function addRecord(record) {
 
 
 
+/*===================================
+=            Holodex API            =
+===================================*/
+
+
 // functions to expose using bridging to HoloDex
 function bridgeIndex(payload) {
   debug(getBridges()[0].CalleeApp);
   debug(payload);
-  return JSON.parse(bridge(
+  return bridge(
     getBridges()[0].CalleeApp,
     "holodex",
     "index",
     payload
-  ));
+  );
 }
 
 function bridgeSearch(payload) {
-  return JSON.parse(bridge(
+  return bridge(
     getBridges()[0].CalleeApp,
     "holodex",
     "search",
     payload
-  ));
+  );
 }
+
+
+// publicly exposed message interface calls
+
+function messageIndex(params) {
+  debug('message index called');
+  var to = params.to,
+      payload = params.payload;
+
+  return JSON.parse(send(to, { call: 'index', payload: payload }));
+}
+
+function messageSearch(params) {
+  var to = params.to,
+      payload = params.payload;
+  return JSON.parse(send(to, { call: 'search', payload: payload }));
+}
+
+/**
+ * receive - Callback for handling direct messages. Used as the main interface between consumer nodes and indexer nodes
+ *
+ * @param      {hash-string}  from - The key hash of the sender
+ * @param      {Object}  msg - The message object
+ * @param      {string} msg.call - The function to call. must be one of 'index', 'search'
+ * @param      {Object} msg.payload - Object to pass to the function call
+ * @return     {Object}  - Returns result of call if valid or an error string
+ */
+function receive(from, msg) {
+  debug('message received');
+  debug(JSON.stringify(msg));
+
+  switch(msg.call) {
+    case 'index':
+      return bridgeIndex(msg.payload);
+      break;
+    case 'search':
+      return bridgeSearch(msg.payload);
+      break;
+    default:
+      return "InvalidCall";
+  }
+}
+
+
+/*=====  End of Holodex API  ======*/
+
+
 
 
 /*=================================
