6154a534340180c175c5d48bef2a493c4f9778c4
Willem Olding <willemolding@gmail.com>
2018-07-03 13:59:30 -0600

    adds addRecordAndIndex public zome function to consumer

--- src_dna/consumer/consumer.js
+++ src_dna/consumer/consumer.js
@@ -2,3 +2,9 @@
 function addRecord(record) {
   return commit('record', record);
 }
+
+function addRecordAndIndex(record) {
+  let hash = commit('record', record);
+  call("holodex", "index", {entryType: 'record', entryHash: hash});
+  return hash;
+}
