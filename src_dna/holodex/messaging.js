23a5ab039757ad9ed5aa426bb74dd78615896f0e
Willem Olding <willemolding@gmail.com>
2018-07-03 11:17:20 -0600

    adds receive callback

---
+++ src_dna/holodex/messaging.js
@@ -0,0 +1,30 @@
+
+/*===========================================
+=            Messaging Callbacks            =
+===========================================*/
+
+/**
+ * receive - Callback for handling direct messages. Used as the main interface between consumer nodes and indexer nodes
+ *
+ * @param      {hash-string}  from - The key hash of the sender
+ * @param      {Object}  msg - The message object
+ * @param      {string} msg.call - The function to call. must be one of 'index', 'search'
+ * @param      {Object} msg.payload - Object to pass to the function call
+ * @return     {Object}  - Returns result of call if valid or an error string
+ */
+function receive(from, msg) {
+  switch(msg.call) {
+    case 'index':
+      return index(msg.payload);
+      break;
+    case 'search':
+      return search(msg.payload);
+      break;
+    default:
+      return "InvalidCall";
+  }
+}
+
+/*=====  End of Messaging Callbacks  ======*/
+
+
